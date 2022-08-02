7e0d217b3d85a82ce7a768d2db4bd68cd7628430
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-02 23:03:24 +0500

    Completed Calculator Design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i += 1) {
+      digits.push(
+        <button type="button" key={i}>{i}</button>,
+      );
+    }
+    return digits;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          0
+        </div>
+
+        <div className="operators">
+          <button type="button">/</button>
+          <button type="button">*</button>
+          <button type="button">+</button>
+          <button type="button">-</button>
+
+          <button type="button">AC</button>
+        </div>
+
+        <div className="digits">
+          { createDigits() }
+          <button type="button">0</button>
+          <button type="button">.</button>
+
+          <button type="button">=</button>
+        </div>
+      </div>
+
     </div>
   );
 }
