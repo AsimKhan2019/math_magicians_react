c1ed94469ef48c75293e65a7ab439cde57e2ebdb
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-04 16:18:42 +0500

    Applied useState hook to the code

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,76 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.showAnswer = this.showAnswer.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  handleClick = (e) => {
-    const answer = calculate(this.state, e.target.textContent);
-    this.setState(answer);
+  const handleClick = (e) => {
+    const answer = calculate(state, e.target.textContent);
+    setState(answer);
   };
 
-  showAnswer = (e) => {
-    this.setState({ total: e.target.textContent });
+  const showAnswer = (e) => {
+    setState({ total: e.target.textContent });
   };
 
-  render() {
-    // reference as a js object
-    const createDigits = () => {
-      const digits = [];
+  const createDigits = () => {
+    const digits = [];
 
-      for (let i = 1; i < 10; i += 1) {
-        digits.push(
-          <button
-            type="button"
-            onClick={this.handleClick}
-            key={i}
-          >
-            {i}
-          </button>,
-        );
-      }
-      return digits;
-    };
+    for (let i = 1; i < 10; i += 1) {
+      digits.push(
+        <button
+          type="button"
+          onClick={handleClick}
+          key={i}
+        >
+          {i}
+        </button>,
+      );
+    }
+    return digits;
+  };
 
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <div className="display" onChange={this.showAnswer}>
-          {total}
-          {operation}
-          {next}
-        </div>
-
-        <div className="simpleOperators">
-          <button type="button" onClick={this.handleClick}>AC</button>
-          <button type="button" onClick={this.handleClick}>+/-</button>
-          <button type="button" onClick={this.handleClick}>%</button>
-        </div>
-        <div className="operators">
-          <button type="button" onClick={this.handleClick}>&#247;</button>
-          <button type="button" onClick={this.handleClick}>x</button>
-          <button type="button" onClick={this.handleClick}>-</button>
-          <button type="button" onClick={this.handleClick}>+</button>
-          <button type="button" onClick={this.handleClick}>=</button>
-        </div>
-
-        <div className="digits">
-          { createDigits() }
-          <button type="button" onClick={this.handleClick}>0</button>
-          <button type="button" onClick={this.handleClick}>.</button>
-        </div>
+  const { total, next, operation } = state;
+  return (
+    <div className="calculator">
+      <div className="display" onChange={showAnswer}>
+        {total}
+        {operation}
+        {next}
       </div>
-    );
-  }
-}
+
+      <div className="simpleOperators">
+        <button type="button" onClick={handleClick}>AC</button>
+        <button type="button" onClick={handleClick}>+/-</button>
+        <button type="button" onClick={handleClick}>%</button>
+      </div>
+      <div className="operators">
+        <button type="button" onClick={handleClick}>&#247;</button>
+        <button type="button" onClick={handleClick}>x</button>
+        <button type="button" onClick={handleClick}>-</button>
+        <button type="button" onClick={handleClick}>+</button>
+        <button type="button" onClick={handleClick}>=</button>
+      </div>
+
+      <div className="digits">
+        { createDigits() }
+        <button type="button" onClick={handleClick}>0</button>
+        <button type="button" onClick={handleClick}>.</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator; // export Calculator component
