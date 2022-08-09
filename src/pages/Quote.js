82667586ac6b34f1e991dd09b1ad38b4e28bb98d
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-09 22:02:50 +0500

    Applied Routing to the project

---
+++ src/pages/Quote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="home">
+    <br />
+    <br />
+
+    <br />
+    <br />
+
+    <p>
+      Mathematicians is not about numbers, equations, computations, or algorithims:
+      it is about understanding. - William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
