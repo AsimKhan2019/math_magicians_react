c80660791cade49b821b059cc5bf10907c537b22
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-10 19:55:00 +0500

    Fixed ESLinter errors

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,54 +1,53 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import Calculator from '../components/calculator';
 import Renderer from 'react-test-renderer';
+import Calculator from '../components/calculator';
 
 describe('Calculator', () => {
   it('Renders Calculator', () => {
-    const renderobject = Renderer.create(<Calculator/>).toJSON();
+    const renderobject = Renderer.create(<Calculator />).toJSON();
     expect(renderobject).toMatchSnapshot();
   });
 
   it('Adding two numbers', () => {
-    render(<Calculator/>);
-    fireEvent.click(screen.getByRole('button', { name: '7'}));
-    fireEvent.click(screen.getByRole('button', { name: '+'}));
-    fireEvent.click(screen.getByRole('button', { name: '6'}));
-    fireEvent.click(screen.getByRole('button', { name: '='}));
+    render(<Calculator />);
+    fireEvent.click(screen.getByRole('button', { name: '7' }));
+    fireEvent.click(screen.getByRole('button', { name: '+' }));
+    fireEvent.click(screen.getByRole('button', { name: '6' }));
+    fireEvent.click(screen.getByRole('button', { name: '=' }));
 
     expect(screen.getByText('13')).toBeTruthy();
   });
 
   it('Subtracting two numbers', () => {
-    render(<Calculator/>);
-    fireEvent.click(screen.getByRole('button', { name: '2'}));
-    fireEvent.click(screen.getByRole('button', { name: '6'}));
-    fireEvent.click(screen.getByRole('button', { name: '-'}));
-    fireEvent.click(screen.getByRole('button', { name: '6'}));
-    fireEvent.click(screen.getByRole('button', { name: '='}));
+    render(<Calculator />);
+    fireEvent.click(screen.getByRole('button', { name: '2' }));
+    fireEvent.click(screen.getByRole('button', { name: '6' }));
+    fireEvent.click(screen.getByRole('button', { name: '-' }));
+    fireEvent.click(screen.getByRole('button', { name: '6' }));
+    fireEvent.click(screen.getByRole('button', { name: '=' }));
 
     expect(screen.getByText('20')).toBeTruthy();
   });
 
   it('Multiplying two numbers', () => {
-    render(<Calculator/>);
-    fireEvent.click(screen.getByRole('button', { name: '2'}));
-    fireEvent.click(screen.getByRole('button', { name: 'x'}));
-    fireEvent.click(screen.getByRole('button', { name: '6'}));
-    fireEvent.click(screen.getByRole('button', { name: '='}));
+    render(<Calculator />);
+    fireEvent.click(screen.getByRole('button', { name: '2' }));
+    fireEvent.click(screen.getByRole('button', { name: 'x' }));
+    fireEvent.click(screen.getByRole('button', { name: '6' }));
+    fireEvent.click(screen.getByRole('button', { name: '=' }));
 
     expect(screen.getByText('12')).toBeTruthy();
   });
 
   it('Dividing two numbers', () => {
-    render(<Calculator/>);
-    fireEvent.click(screen.getByRole('button', { name: '1'}));
-    fireEvent.click(screen.getByRole('button', { name: '0'}));
-    fireEvent.click(screen.getByRole('button', { name: '÷'}));
-    fireEvent.click(screen.getByRole('button', { name: '5'}));
-    fireEvent.click(screen.getByRole('button', { name: '='}));
+    render(<Calculator />);
+    fireEvent.click(screen.getByRole('button', { name: '1' }));
+    fireEvent.click(screen.getByRole('button', { name: '0' }));
+    fireEvent.click(screen.getByRole('button', { name: '÷' }));
+    fireEvent.click(screen.getByRole('button', { name: '5' }));
+    fireEvent.click(screen.getByRole('button', { name: '=' }));
 
     expect(screen.getAllByText('2')).toBeTruthy();
   });
-
 });
